0db511867888e392efff49059d8a92830b129809
Akash Jindal <84064774+akash-246@users.noreply.github.com>
2021-07-27 22:35:57 +0530

    Create functions calling and  declaration.c

---
+++ functions calling and  declaration.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void message(); /*function prototype declaration*/
+int main()
+{
+ message(); /*function call*/
+ printf("This is designed by Nikhil Bhardwaj \n");
+ printf("Cry,and you stop monotony! \n");
+ return 0;
+}
+ 
