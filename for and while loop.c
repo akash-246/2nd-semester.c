4f484b42ae83117caba1fdef5168ecea94cbf47c
Akash Jindal <84064774+akash-246@users.noreply.github.com>
2021-07-27 22:49:51 +0530

    Create for and while loop.c

---
+++ for and while loop.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+  
+int main()
+{
+  
+    int i = 0;
+  
+    for (i = 5; i < 10; i++) {
+        printf("GFG\n");
+    }
+  
+    return 0;
+}
+
+
+
+//while loop
+#include <stdio.h>
+  
+int main()
+{
+  
+    int i = 5;
+  
+    while (i < 10) {
+        printf("GFG\n");
+        i++;
+    }
+  
+    return 0;
+}
